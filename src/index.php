5c9d24181e41c6adc34e3556977c4e65e45ee525
Shvan Sheikha <shvansheikha@gmail.com>
2024-01-08 16:37:06 +0330

    get players vehicle and distance

--- src/index.php
+++ src/index.php
@@ -8,6 +8,11 @@ function readTheJSONFile()
     return json_decode($json, true);
 }
 
+function calculateTime($vehicle, $distance)
+{
+    return $distance / $vehicle['maxSpeed'] * 1000;
+}
+
 $vehiclesList = readTheJSONFile();
 
 $vehiclesListSize = count($vehiclesList);
@@ -16,3 +21,9 @@ for ($i = 0; $i <= $vehiclesListSize - 1; $i++) {
     echo "$i:" . $vehiclesList[$i]['name'] . "\n";
 }
 
+$message = "Player %s choose an item between 0-" . ($vehiclesListSize - 1);
+
+$playerOneVehicle = cli\prompt(sprintf($message, 'One'), $default = false, $marker = ':');
+$playerTwoVehicle = cli\prompt(sprintf($message, 'Two'), $default = false, $marker = ':');
+
+$distance = cli\prompt("Input Distance", $default = false, $marker = ':');
