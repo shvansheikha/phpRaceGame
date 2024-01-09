a187909a907180b858e3c49a54eb8ceaca86452c
Shvan Sheikha <shvansheikha@gmail.com>
2024-01-09 11:16:01 +0330

    move strings to strings utility class

---
+++ src/Utilities/Strings.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Utilities;
+
+class Strings
+{
+    public static string $VEHICLES_LIST_MESSAGE = "%s : %s";
+    public static string $QUESTION_CHOSE_MESSAGE = "Player %s choose an item between 0-%s";
+    public static string $QUESTION_INPUT_DISTANCE = "Input Distance";
+    public static string $QUESTION_PLAY_AGAIN = "do you want to play again?";
+}
